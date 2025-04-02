6f4d19cca07ac6984d930cae9b91698c803b5ba9
wattleflow <wattleflow@outlook.com>
2025-04-02 17:07:40 +1100

    Added: __init__

---
+++ pathadder/__init__.py
@@ -0,0 +1,15 @@
+# Module Name: concrete/blackboard.py
+# Description: This modul contains concrete blackboard classes.
+# Author: (wattleflow@outlook.com)
+# Copyright: (c) 2022-2024 WattleFlow
+# License: Apache 2 Licence
+
+from .pathadder import (
+    override_paths,
+    show_paths
+)
+
+__all__ = [
+    "override_paths",
+    "show_paths"
+]
